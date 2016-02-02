eaee477fc3bbe3ca912d2d390ad02c4aa66208ea
Forrest York <badisa@users.noreply.github.com>
2016-02-02 11:49:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='Nexys-UCF-Script',
     version="0.0.1",
-    description="A script for creating UFC files for Nexys 3 Boards",
+    description="A script for creating UCF files for Nexys 3 Boards",
     long_description=open("README.rst").read(),
     url="https://github.com/badisa/Nexys-3-Script/",
     author="Forrest York",
