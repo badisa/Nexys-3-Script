4b94a9cbc1a50ce5b89127a445899cced73211e0
Forrest York <forrest.york@gmail.com>
2016-02-02 11:33:25 -0700

    Updates setup.py file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     description="A script for creating UFC files for Nexys 3 Boards",
     long_description=open("README.rst").read(),
-    url="https://github.com/badisa/nexys_3_ufc_script",
+    url="https://github.com/badisa/Nexys-3-Script/",
     author="Forrest York",
     author_email="forrest.york@gmail.com",
     license="MIT",
